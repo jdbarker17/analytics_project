be3d388d670f04527920fdb2ee3156703a42a9a0
Jon Barker <jon@Jons-MBP.lan>
2023-09-13 21:55:54 -0400

    macbook mods and config

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from youtube_analytics import  *
+from data_processing import *
 import pandas as pd
 
+
 def main():
-    creds = authenticate_OAUTH2()
-    output_dataframe = get_all_video_data(creds)
-    output_dataframe.to_excel('output.xlsx')
+    #creds = authenticate_OAUTH2()
+    #output_dataframe = get_all_video_data(creds)
+    #output_dataframe.to_excel('output.xlsx')
+    output_dataframe = pd.read_excel('output.xlsx')
+    visualize(output_dataframe)
 
     #Processing Goes Below. Data Processing, Database management, Optimizations probably down the road.
 
