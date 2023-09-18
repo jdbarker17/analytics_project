ad7dedc707dc94861c6e8d1bf3acaf48e9e8f717
Jon Barker <jdbarker17@gmail.com>
2023-09-18 11:08:19 -0500

    Column Plotting

--- data_processing.py
+++ data_processing.py
@@ -1,24 +1,43 @@
 import matplotlib.pyplot as plt
 import numpy as np
+import math
 
-def visualize(df,video_ids):
-    fig, axs = plt.subplots(len(video_ids))
-    ticks = np.array([15,45,73,104,134,165,195,226])
-    labels = ['Jan','Feb','Mar','April','May','June','July','Aug']
-    print(f"Video IDs = {video_ids}")
-    for id in enumerate(video_ids):
-        print(f"Id = {id}")
-        subset = df[df['video_id'] == id[1][0]]
-        #subset['cumsum'] = subset['views'].cumsum()
-        subset.head()
-        axs[id[0]].plot(subset['views'])
-        #axs[id[0]].plot(subset['views'].cumsum())
-        axs[id[0]].set_title(f"Video_Title: {id[1][1]}")
-        axs[id[0]].set_xticks(ticks, minor=False)
-        axs[id[0]].set_xticklabels(labels, fontdict=None, minor=False)
-        #Ticks should be relatively accurate. confirm this 
-        #TODO
-        ticks = ticks + 243
-        #axs[id[0]].xticks(ticks= ticks, labels=labels, **kwargs)
+
+def visualize(df, video_ids):
+    # Calculate number of rows and columns for the grid
+    n = len(video_ids)
+    rows = int(math.sqrt(n))
+    cols = int(math.ceil(n / rows))
     
+    fig, axs = plt.subplots(rows, cols, figsize=(15, 15))
+    
+    # Check if we have a single row or column
+    if rows == 1 or cols == 1:
+        axs = np.reshape(axs, (-1, ))
+    
+    #Ticks to set Lower Axis
+    ticks = np.array([15, 45, 73, 104, 134, 165, 195, 226])
+    labels = ['Jan', 'Feb', 'Mar', 'April', 'May', 'June', 'July', 'Aug']
+    print(f"Video IDs = {video_ids}")
+    
+    for idx, (video_id, video_title) in enumerate(video_ids):
+        subset = df[df['video_id'] == video_id]
+        row = idx // cols
+        col = idx % cols
+        
+        axs[row, col].plot(subset['views'])
+        axs[row, col].set_title(f"Video_Title: {video_title}")
+        axs[row, col].set_xticks(ticks, minor=False)
+        axs[row, col].set_xticklabels(labels, fontdict=None, minor=False)
+        
+        # Update ticks for the next plot
+        ticks = ticks + 243
+        
+    # Remove any unused subplots
+    for i in range(idx + 1, rows * cols):
+        row = i // cols
+        col = i % cols
+        fig.delaxes(axs[row, col])
+    
+    plt.tight_layout()
     plt.show()
