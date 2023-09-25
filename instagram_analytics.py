2dcba5008fb204e0cbde4b176b7ca4ae337e6a38
Jon Barker <jdbarker17@gmail.com>
2023-09-25 18:10:19 -0400

    updated API call. Still not functioning :/

--- instagram_analytics.py
+++ instagram_analytics.py
@@ -6,6 +6,7 @@
 from flask import Flask, request, redirect, session
 import secret
 import requests
+import json
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # for session
@@ -36,10 +37,71 @@ def callback():
     access_token = data['access_token']
     session['access_token'] = access_token
     print(f'Logged in with access token: {access_token}')
-    
-    return f'Logged in with access token: {access_token}'
+
+    return redirect('/get_instagram_id')
+    #return f'Logged in with access token: {access_token}'
 
 # Add additional routes to make API calls for Instagram data using the access token
+
+@app.route('/get_instagram_id')
+def get_instagram_id():
+    access_token = session.get('access_token')
+    print(f'Access Token = {access_token}')
+    if not access_token:
+        return "Access token not found. Please authenticate first."
+
+    # Fetch the User's Pages
+    pages_url = f'https://graph.facebook.com/v12.0/me/accounts?access_token={access_token}'
+    response = requests.get(pages_url)
+    data = response.json()
+    print(json.dumps(data))
+
+    if 'data' in data:
+        debug_info = []  # List to store debug information
+        for page in data['data']:
+            page_id = page['id']
+            page_access_token = page['access_token']
+
+            # For each Page, try to fetch the associated Instagram account ID
+            instagram_url = f'https://graph.facebook.com/v12.0/{page_id}?fields=instagram_business_account&access_token={page_access_token}'
+            insta_response = requests.get(instagram_url)
+            insta_data = insta_response.json()
+
+            debug_info.append(insta_data)  # Append the response to debug info
+
+            if 'instagram_business_account' in insta_data:
+                instagram_id = insta_data['instagram_business_account']['id']
+                return f'Instagram Creator Account ID: {instagram_id}'
+
+        # Return the debug information for inspection
+        return f"No associated Instagram Creator Account found. Debug Info: {debug_info}"
+    else:
+        error_message = data.get('error', {}).get('message', 'Unknown error.')
+        return f"Error fetching User's Pages: {error_message}"
+    
+    
+
+@app.route('/get_instagram_data')
+def get_instagram_data():
+    access_token = session.get('access_token')
+    if not access_token:
+        return "Access token not found. Please authenticate first."
+
+    # Fetch the Instagram Business Account ID
+    me_url = f'https://graph.facebook.com/v12.0/me?fields=instagram_business_account&access_token={access_token}'
+    response = requests.get(me_url)
+    data = response.json()
+    instagram_id = data['instagram_business_account']['id']
+
+    # Fetch recent media objects from the Instagram Business Account
+    media_url = f'https://graph.facebook.com/v12.0/{instagram_id}/media?access_token={access_token}'
+    media_response = requests.get(media_url)
+    media_data = media_response.json()
+
+    return media_data
+
+# ... [Your existing code to run the app]
+
 
 if __name__ == '__main__':
     app.run(debug=True)
